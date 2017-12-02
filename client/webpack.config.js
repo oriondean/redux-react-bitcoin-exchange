b38277e9be536c53f34f39d1cb4de9ae71cc1aa6
Dean Kerr <dean_k@live.co.uk>
2017-12-02 21:37:27 +0000

    update webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,45 +1,48 @@
-var path = require('path');
-var scripts = path.resolve(__dirname, "app/scripts");
+const path = require('path');
+
+const scripts = path.resolve(__dirname, 'app/scripts');
 
 module.exports = {
-    entry: [
-        './app/scripts/index.js'
+  entry: path.resolve(scripts, 'index.jsx'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'eslint-loader',
+        include: scripts,
+        enforce: 'pre',
+      },
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        include: scripts,
+        options: {
+          presets: ['es2015', 'react'],
+        },
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+        include: scripts,
+      },
+      {
+        test: /\.scss$/,
+        loader: ['style-loader', 'css-loader', 'sass-loader'],
+        include: scripts,
+      },
+      {
+        test: /\.css$/,
+        loader: ['style-loader', 'css-loader'],
+        include: path.resolve(__dirname, 'app/css'),
+      },
     ],
-    output: {
-        path: './build',
-        filename: 'bundle.js'
-    },
-    devtool: 'source-map',
-    module: {
-        preLoaders: [
-            {
-                test: /\.js$/,
-                loaders: ['eslint'],
-                include: scripts
-            }
-        ],
-        loaders: [
-            {
-                test: /\.js$/,
-                loader: 'babel',
-                include: scripts,
-                query: { presets: ['es2015', 'react'] }
-            },
-            {
-                test: /\.html$/,
-                loader: 'html',
-                include: scripts
-            },
-            {
-                test: /\.scss$/,
-                loaders: ['style', 'css', 'sass'],
-                include: scripts
-            },
-            {
-                test: /\.css$/,
-                loaders: ['style', 'css'],
-                include: path.resolve(__dirname, "app/css")
-            }
-        ]
-    }
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.css', '.scss'],
+  },
 };
