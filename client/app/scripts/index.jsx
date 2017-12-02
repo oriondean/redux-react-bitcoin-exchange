73d9c5c7afd1cbc78ee8e79b047bdc4731d33147
Dean Kerr <dean_k@live.co.uk>
2017-12-02 21:38:36 +0000

    fix incorrect capitalisation in app import

--- client/app/scripts/index.jsx
+++ client/app/scripts/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import App from './containers/App';
+import App from './containers/app';
 import configureStore from './store/store';
 
 import '../css/app.css';
